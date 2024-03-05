d5eb32705578cfdd8b33689102ccaa29bc5b9a14
VITTALDEVARAJU <vittaldevaraju65@gmail.com>
2024-03-05 15:44:26 +0530

    It is my core java pratical programs

---
+++ Corejava_MVIT/src/Day_1/Samplejava.java
@@ -0,0 +1,17 @@
+package Day_1;
+import java.util.*;
+public class Samplejava {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("Enter the a value:");
+		a=s.nextInt();
+		System.out.println("Enter the b value:");
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a:"+a+" and b:"+b+" is:"+c);
+}
+
+}
+
