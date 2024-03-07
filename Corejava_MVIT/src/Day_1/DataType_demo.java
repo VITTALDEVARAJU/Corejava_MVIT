22030ba0fc46cd1cffafbd2a0bc83d2ec731d821
VITTALDEVARAJU <vittaldevaraju65@gmail.com>
2024-03-07 11:03:44 +0530

    This is my basic java core program

---
+++ Corejava_MVIT/src/Day_1/DataType_demo.java
@@ -0,0 +1,24 @@
+package Day_1;
+
+public class DataType_demo {
+
+	public static void main(String[] args) {
+		int value1 = 9/2;
+		float value2 = 101/61;
+		double value3 = 10d/6d;
+		float value4 = 5/2.5f;
+		System.out.println("Value of 1 = "+value1);
+		System.out.println("Value of 2 = "+value2);
+		System.out.println("Value of 3 = "+value3);
+		System.out.println("Value of 4 = "+value4);
+		//assigning exp to var
+		int marker = 512;
+		double percentage = marker*2;
+		System.out.println("percentage: "+percentage);
+		int a = 6;
+		float b = a*a;
+		System.out.println("Ans is: "+b);
+
+	}
+
+}
---
+++ Corejava_MVIT/src/Day_1/Identifier_demo.java
@@ -0,0 +1,13 @@
+package Day_1;
+
+public class Identifier_demo {
+
+	public static void main(String[] args) {
+		int $number = 2;
+		System.out.println(+$number);
+		int n = 4;
+		System.out.print(+n);
+
+	}
+
+}
